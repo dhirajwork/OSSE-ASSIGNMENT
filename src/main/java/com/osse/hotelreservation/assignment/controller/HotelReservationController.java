7ffa090f13c72ab10c20cf27c7ff34e12d04c9a8
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2021-12-16 18:24:17 +0530

    Update HotelReservationController.java

--- src/main/java/com/osse/hotelreservation/assignment/controller/HotelReservationController.java
+++ src/main/java/com/osse/hotelreservation/assignment/controller/HotelReservationController.java
@@ -10,11 +10,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Controller
 public class HotelReservationController {
+
+    private UserService userService;
 
     private ReservationService reservationService;
 
+    @Autowired
+    public HotelReservationController(UserService userService, ReservationService reservationService) {
+        this.userService = userService;
+        this.reservationService = reservationService;
+    }
 
     // data binder
     @InitBinder
@@ -28,6 +51,45 @@ public class HotelReservationController {
     public String homePage() {
 
         return "home-page";
+    }
+
+    // login page
+    @GetMapping("/login-form-page")
+    public String loginPage(Model model) {
+
+        // if user is already login, redirect to home
+
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/";
+        }
+
+        // new user attribute for sign up page
+        model.addAttribute("newUser", new CurrentUser());
+
+        return "login";
+    }
+
+    // registration process page
+    @PostMapping("/processRegistration")
+    public String processRegistrationForm(@Valid @ModelAttribute("newUser") CurrentUser currentUser,
+                                          BindingResult theBindingResult, Model model) {
+
+        // check the database if user already exists
+        if (userService.findUserByEmail(currentUser.getEmail()) != null) {
+            model.addAttribute("newUser", new CurrentUser());
+            model.addAttribute("registrationError", "Email already exists.");
+
+            return "login";
+        }
+
+        // create user account
+        userService.saveUser(currentUser);
+        model.addAttribute("registrationSuccess", "registration Success.");
+
+        return "redirect:/login-form-page";
+
     }
 
     // booking page
@@ -60,6 +122,17 @@ public class HotelReservationController {
         return "your-reservations";
     }
 
+    // update reservation
+    @PostMapping("/reservation-update")
+    public String updateReservation(@RequestParam("resId") int resId, Model model) {
+
+        // new update reservation sent to services to store it in database
+        model.addAttribute("newRes", reservationService.reservationToCurrentReservationById(resId));
+
+        return "reservation-page";
+    }
+
+
     // delete reservation
     @PostMapping("/reservation-delete")
     public String deleteReservation(@RequestParam("resId") int resId) {
@@ -68,6 +141,19 @@ public class HotelReservationController {
         reservationService.deleteReservation(resId);
 
         return "redirect:/your-reservations";
+    }
+
+    // log out
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+
+        // handle logout for logged user
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login-form-page?logout";
     }
 
 }
